59275f8994e1dd588dd8ef1773985140fb28f47f
lsj <936339745@qq.com>
2016-03-14 23:27:30 +0800

    2016-3-14 服务器测试

--- lsjnews/src/main/java/com/lsj/lsjnews/activity/lsj_test1.java
+++ lsjnews/src/main/java/com/lsj/lsjnews/activity/lsj_test1.java
@@ -1,26 +1,53 @@
 package com.lsj.lsjnews.activity;
 
+import com.alibaba.fastjson.JSON;
+import com.bumptech.glide.request.Request;
 import com.example.lsj.httplibrary.base.BaseActivity;
+import com.example.lsj.httplibrary.utils.MyToast;
 import com.lsj.lsjnews.R;
+import com.lsj.lsjnews.base.LsjBaseCallBack;
 import com.lsj.lsjnews.view.LsjLoadingView;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created by lsj on 2016/3/7.
  */
 public class lsj_test1 extends BaseActivity{
-    LsjLoadingView mLoading;
 
     @Override
     protected void initView() {
         super.initView();
-        mLoading = (LsjLoadingView) findViewById(R.id.loading);
     }
 
     @Override
     protected void initData() {
         showTopView(false);
-        mLoading.startLoadingAnim();
+        RequestParams params = new RequestParams("http://182.254.145.222/lsj/mdnews/configs/mysql.func.php");
+        params.addBodyParameter("username", "李上健");
+        x.http().post(params, new Callback.CommonCallback<String>() {
 
+            @Override
+            public void onSuccess(String s) {
+                MyToast.showToast(mContext,":"+s);
+            }
+
+            @Override
+            public void onError(Throwable throwable, boolean b) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException e) {
+
+            }
+
+            @Override
+            public void onFinished() {
+            }
+        });
     }
 
     @Override
